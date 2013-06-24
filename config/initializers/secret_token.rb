0827c0baddbf27b544e92032d14d45f63dd72c5e
Margaret Scott <eudaimonious@gmail.com>
2013-06-24 16:17:14 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hang::Application.config.secret_token = 'f4e4f2632aa68d97969fddc80eecad225092b4af5f2acf67c7cf462457d760aa9004c31304fb7644e978c34174fcf1a6bcf74dd37cd03a5c5d7a3bf93bb35cc3'
